8ab04ddfa9dce37884bdd2069e7c1b0ed0da1c13
kufirre <kufy@yourduino.com>
2019-10-11 17:37:00 +0100

    file to measure the speed encoder counts

---
+++ SpeedEncoderTest.ino/SpeedEncoderTest.ino.ino
@@ -0,0 +1,32 @@
+const byte LEFT_OPTOINTERRUPTER_PIN  =    2;  // The optoInterrupters for encoder discs
+const byte RIGHT_OPTOINTERRUPTER_PIN =    3;
+
+volatile unsigned int counter_left;
+volatile unsigned int counter_right;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LEFT_OPTOINTERRUPTER_PIN, INPUT);
+  pinMode(RIGHT_OPTOINTERRUPTER_PIN, INPUT);
+  
+  digitalWrite(LEFT_OPTOINTERRUPTER_PIN, HIGH);
+  digitalWrite(RIGHT_OPTOINTERRUPTER_PIN, HIGH);
+  
+  attachInterrupt(digitalPinToInterrupt(LEFT_OPTOINTERRUPTER_PIN), countpulse_left, RISING);
+  attachInterrupt(digitalPinToInterrupt(RIGHT_OPTOINTERRUPTER_PIN), countpulse_right, RISING);
+
+}
+
+void countpulse_left() {
+  counter_right++;
+}
+
+void countpulse_right() {
+  counter_left++;
+}
+
+void loop() {
+  Serial.print("LEFT_COUNT is "  + String(counter_left) + '\n');
+  Serial.println("RIGHT_COUNT is "  + String(counter_right) + '\n');
+}
